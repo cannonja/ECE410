005802f8c4e92ea4818a649c66629e4eaf538295
cannonja <jack_cannon@live.com>
2016-05-05 18:41:13 -0700

    blah

--- theta_project/task1.m
+++ theta_project/task1.m
@@ -1,31 +1,26 @@
 %%Task 1 -- need to ask about how to get deltas without solving for them
 
-%{
+
 S = 200;
 DTE = 45;
 IV = 0.18;
 rf = 0.03;
 
 strikes = [180:5:220];
-list = [];
+data = [];
 
 for i = strikes
     [~, ~, cd, ~] = blackscholes(S, i, rf, DTE/365, IV);
-    list = [list cd];
+    data = [data; i cd];
 end
 
-strikes = transpose(strikes);
-list = transpose(list);
-
-T = table(strikes, list);
-T
-
-[strikes list]
-%}
+digits(3);
+double(data);
+data
 
 
 %%%Task 2
-
+%{
 K = 200;
 S = [170:230];
 DTE = [45 30 15 .0001];
@@ -49,14 +44,14 @@ xlabel('Underlying Price');
 ylabel('Option Value');
 legend(p, '45 DTE', '30 DTE', '15 DTE', '0 DTE');
 %pause
-
+%}
 
 
 %%%Task 3
 %{
 K = 200;
 S = 200;
-DTE = [45:-1:1 .01];
+DTE = [45:-0.1:0.1 .01];
 IV = 0.18;
 rf = 0.03;
 y = [];
@@ -69,8 +64,12 @@ end
 
 
 figure
-p = plot(transpose(DTE), y);
+p = plot(DTE, y);
+set(gca, 'xdir', 'reverse');
 title('Option Value Over Time');
 xlabel('DTE');
 ylabel('Option Value');
 %}
+
+
+
