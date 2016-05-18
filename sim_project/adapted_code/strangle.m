fa526994228302810c0255afb43871cdafaeb221
Jack Cannon <jc2@ds.cecs.pdx.edu>
2016-05-17 22:25:39 -0700

    Added extra strike argument

--- sim_project/adapted_code/strangle.m
+++ sim_project/adapted_code/strangle.m
@@ -1,4 +1,4 @@
-function strangle_price = strangle(S, K, r, sigma, time)
-call_price = bs_european_call(S, K, r, sigma, time);
-put_price = bs_european_put(S, K, r, sigma, time);
+function strangle_price = strangle(S, Kc, Kp, r, sigma, time)
+call_price = bs_european_call(S, Kc, r, sigma, time);
+put_price = bs_european_put(S, Kp, r, sigma, time);
 strangle_price = call_price + put_price;
