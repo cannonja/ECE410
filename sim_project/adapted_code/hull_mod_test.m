4081152c9c927b4075b8c9710cf36ffdaaff8a0e
Jack Cannon <jc2@ds.cecs.pdx.edu>
2016-05-18 05:45:44 -0700

    Finished test script

---
+++ sim_project/adapted_code/hull_mod_test.m
@@ -0,0 +1,35 @@
+deltas = [0.0:0.05:1.0];
+S = 200;
+rf = 0.03;
+DTE = 45;
+IV = 0.3;
+data = [];
+
+for i = deltas
+    Kc = hull_call_mod(S, rf, DTE/360, IV, i);
+    Cp = bs_european_call(S, Kc, rf, IV, DTE/360);
+    Kp = hull_put_mod(S, rf, DTE/360, IV, -i);
+    Pp = bs_european_put(S, Kp, rf, IV, DTE/360);
+    data = [data; [Cp, Kc, Pp, Kp]];
+end
+
+T = table(data(:,1), data(:,2), data(:,3), data(:,4), 'VariableName',...
+        {'CallP', 'CallK', 'PutP', 'PutK'});
+
+disp('-----------hull_modified results-------------');
+T
+
+%%% Use strikes from above to generate prices and compare
+
+hull_data = [];
+for i = 1:length(data(:,2))
+    cp = bs_european_call(S, data(i,2), rf, IV, DTE/360);
+    pp = bs_european_put(S, data(i,4), rf, IV, DTE/360);
+    hull_data = [hull_data; [cp, pp]];
+end
+
+T2 = table(data(:,1), hull_data(:,1), data(:,3), hull_data(:,2),...
+        'VariableName', {'Mod_call', 'Hull_call', 'Mod_put', 'Hull_put'});
+
+disp('------------------Comparison---------------------------');
+T2
