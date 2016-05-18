49ea2c165ec83e158ff0cc8eabf5ce5443e8f818
Jack Cannon <jc2@ds.cecs.pdx.edu>
2016-05-17 22:16:33 -0700

    Changed straddle to strangle in code

--- sim_project/adapted_code/strangle.m
+++ sim_project/adapted_code/strangle.m
@@ -1,4 +1,4 @@
-function straddle_price = straddle(S, K, r, sigma, time)
+function strangle_price = strangle(S, K, r, sigma, time)
 call_price = bs_european_call(S, K, r, sigma, time);
 put_price = bs_european_put(S, K, r, sigma, time);
-straddle_price = call_price + put_price;
+strangle_price = call_price + put_price;
